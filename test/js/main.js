23c12cacf98834bed5f0a9160896a9ef9b521691
origiboy <origiboy@yandex.ru>
2021-01-25 19:20:15 +0300

    1

--- test/js/main.js
+++ test/js/main.js
@@ -1,5 +1,36 @@
+var flag_menu = 0, flag_popup = 0;
 $(document).ready(function(){
+    $(".burger__mobile").eq(0).click(function() {
+        if (flag_menu == 0) {
+            $(".head__mobile").eq(0).css("left", "0");
+            $(".burger__mobile").eq(0).attr("src", "images/burger_close.svg");
+            flag_menu = 1;
+        } else {
+            $(".head__mobile").eq(0).css("left", "100vw");
+            $(".burger__mobile").eq(0).attr("src", "images/burger.svg");
+            flag_menu = 0;
+        }
+    });
     
+    $(".footer__link").eq(0).mouseup(function() {
+        
+        $(".popup__1").eq(0).css("display", "inline");
+        flag_popup = 1;
+    });
+     $(".footer__link").eq(1).mouseup(function() {
+        $(".popup__2").eq(0).css("display", "inline");
+         flag_popup = 1;
+    });     
+    $(window).mousedown(function()
+                {
+        if (flag_popup == 1)
+            {
+                $(".popup__1").eq(0).css("display", "none");
+                $(".popup__2").eq(0).css("display", "none");
+                flag_popup = 0;
+            }
+    });
+                                     
 });
 $("#polzunok").slider({
         animate: "slow",
