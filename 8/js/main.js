f9734d5a8fc8e3687241397bff92aac5fa7fd834
origiboy <origiboy@yandex.ru>
2021-04-01 21:12:14 +0300

    1

--- 8/js/main.js
+++ 8/js/main.js
@@ -7,11 +7,26 @@ function handlePaste (e) {
 	pastedData = pastedData.replace(/(?:\r\n|\r|\n)/g, "<br>");
 	this.innerHTML += pastedData;
 }
-function handleInput () {
-	let a = this.innerHTML;
+function handleInput (e) {
+    if (e.inputType === "insertFromDrop") {
+        let a = this.innerHTML;
 	a = a.replace(/(?:\r\n|\r|\n)/g, "<br>");
 	a = a.replace(/<(?!br\s*\/?)[^>]+>/g, '');
-	this.innerHTML = a;
+	this.innerHTML = "";
+	this.innerHTML += a;
+	this.innerHTML += "\r\n";
+	/*var el = document.querySelector('[contenteditable]');
+    var range = document.createRange();
+    var sel = window.getSelection();
+    range.setStart(el.childNodes[0], 8);
+    range.collapse(false);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    el.focus();
+	*/
+    }
+	
 }
+
 document.querySelector('[contenteditable]').addEventListener('paste', handlePaste, false);
 document.querySelector('[contenteditable]').addEventListener('input', handleInput, false);
